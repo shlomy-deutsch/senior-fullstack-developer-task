18eb0a2698ea22538e0bf6853c2ef5fde018c7d9
Shlomi Deutsch <s0583285252@gmail.com>
2026-01-06 17:13:15 +0200

    remove hardcoded text

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
+    origin: [process.env.FRONTEND_URL],
     methods: 'GET,POST,DELETE',
     credentials: true
   });
